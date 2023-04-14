733dfd7d9cf8728da339c6d602a5119eeb071c18
Lorena Rojas <lochan04@gmail.com>
2023-04-14 07:56:38 -0500

    added comments

--- Player2.java
+++ Player2.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 public class Player2 extends Player {
     Scanner in = new Scanner(System.in);
 
+    // Sets player 2 name
     public void selectPlayerName() {
         System.out.println("Player 2: Enter your name: ");
         String name = in.nextLine();
         setPlayerName(name);
     }
+
+    // Sets player 2 move
     public void selectPlayerMove() {
         System.out.println("enter your move: rock, paper, or scissors");
         String myMove = in.nextLine();
