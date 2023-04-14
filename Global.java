01fa1afe2c249c61859d24f8a40e0b6b7faa865c
Lorena Rojas <lochan04@gmail.com>
2023-04-14 07:58:08 -0500

    added global variables

---
+++ Global.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Global {
+    // Stores all global variables
+    static Scanner in = new Scanner(System.in);
+    static Player1 player1 = new Player1();
+    static Player2 player2 = new Player2();
+    static Computer computer = new Computer();
+    static String game;
+    static String quitGame;
+}
